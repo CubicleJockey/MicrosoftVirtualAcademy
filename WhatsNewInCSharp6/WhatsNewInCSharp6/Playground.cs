770c5841ff0f77c08396f3c077ac17853a62770c
André Davis <CubicleJockey@users.noreply.github.com>
2015-07-02 12:00:19 -0700

    Getting setup to do the MVA of what's new in C# 6
    Getting setup

---
+++ WhatsNewInCSharp6/WhatsNewInCSharp6/Playground.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WhatsNewInCSharp6
+{
+    [TestClass]
+    public class Playground
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
