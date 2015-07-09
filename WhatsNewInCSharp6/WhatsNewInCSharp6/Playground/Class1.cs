a87e15f4fe392c6429f64610aa85d481aa82798a
André Davis <cubicle.jockey@live.com>
2015-07-09 10:12:19 -0700

    Changing to the Universal Library Type

---
+++ WhatsNewInCSharp6/WhatsNewInCSharp6/Playground/Class1.cs
@@ -0,0 +1,8 @@
+﻿using static System.Math;
+
+namespace Playground
+{
+    public class Test
+    {
+    }
+}
--- WhatsNewInCSharp6/WhatsNewInCSharp6/Playground/Properties/AssemblyInfo.cs
+++ WhatsNewInCSharp6/WhatsNewInCSharp6/Playground/Properties/AssemblyInfo.cs
@@ -5,22 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WhatsNewInCSharp6")]
+[assembly: AssemblyTitle("Playground")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WhatsNewInCSharp6")]
+[assembly: AssemblyProduct("Playground")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a4fde591-1f73-4c3b-baab-99ace1a3c7ac")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,3 +26,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
