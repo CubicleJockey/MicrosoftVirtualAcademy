dbc7199f55fd20e73c3d4b43e8a81fb286dd5532
André Davis <CubicleJockey@users.noreply.github.com>
2017-01-03 14:24:24 -0800

    Json configuration

--- Introduction_To_ASP.NET_CORE_1.0/Basic/WebApp/src/WebApp/Startup.cs
+++ Introduction_To_ASP.NET_CORE_1.0/Basic/WebApp/src/WebApp/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -8,6 +9,16 @@ namespace WebApp
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -35,7 +46,7 @@ namespace WebApp
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync($"App is running in mode: [{env.EnvironmentName}.]");
+                await context.Response.WriteAsync($"App is running in mode: [{env.EnvironmentName} it's something [{Configuration["Message"]}].]");
             });
         }
     }
