e9d67333248c6cc42794b9cac9d8a716e2890c6e
André Davis <CubicleJockey@users.noreply.github.com>
2017-01-03 17:04:13 -0800

    Routing Attributes

---
+++ Introduction_To_ASP.NET_CORE_1.0/Basic/WebApp/src/WebApp/Controllers/FooController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class FooController
+    {
+        [HttpGet("/")]
+        public string Index() => "Hello from MVC, well acutally just the C(Controller)!";
+
+        [HttpGet("/Foo")]
+        public string SomethingElse() => "Hello from Foo.";
+    }
+}
--- Introduction_To_ASP.NET_CORE_1.0/Basic/WebApp/src/WebApp/Startup.cs
+++ Introduction_To_ASP.NET_CORE_1.0/Basic/WebApp/src/WebApp/Startup.cs
@@ -1,12 +1,10 @@
-﻿using System.Text;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Thingy;
 
 namespace WebApp
 {
@@ -26,10 +24,7 @@ namespace WebApp
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //https://github.com/aspnet/StaticFiles/blob/dev/src/Microsoft.AspNetCore.StaticFiles/DirectoryBrowserServiceExtensions.cs
-            //services.AddDirectoryBrowser();
-
-            services.AddRouting();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,33 +37,7 @@ namespace WebApp
                 app.UseDeveloperExceptionPage();
             }
 
-            //https://github.com/aspnet/StaticFiles
-            //Order matters.
-            //app.UseDirectoryBrowser();
-
-            /*Will default to index.html*/
-            //app.UseFileServer(); 
-
-            //Note: Order matters
-            var routeBuilder = new RouteBuilder(app);
-            routeBuilder.MapGet(string.Empty, context => context.Response.WriteAsync("Default route."));
-            routeBuilder.MapGet("Thingy", context => context.Response.WriteAsync("Thingy route."));
-            routeBuilder.MapGet("thingy/stuff", context => context.Response.WriteAsync("Complex Route"));
-            routeBuilder.MapGet("thingy/{id:int}", 
-                                context => context.Response.WriteAsync($"Thingy value: [{context.GetRouteValue("id")}]"));
-
-            app.UseRouter(routeBuilder.Build());
-
-            //app.Run(async (context) =>
-            //{
-            //    var stuff = new Stuff();
-
-            //    var sb = new StringBuilder();
-            //    sb.AppendLine($"App is running in mode: [{env.EnvironmentName} it's something [{Configuration["Message"]}].]");
-            //    sb.AppendLine(stuff.Message);
-
-            //    await context.Response.WriteAsync(sb.ToString());
-            //});
+            app.UseMvc();
         }
     }
 }
